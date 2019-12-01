64a615439c9842750f5ab3449bdf70db4f3e0406
2BAB <xx2bab@gmail.com>
2019-12-01 21:20:53 +0800

    feat: bump gradle version to 3.5.2 on core project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ buildscript {
     // Set project ext values as the workaround to collect all values that can't be set in buildSrc,
     // because buildscript can not read anything from the scripts(buildSrc) that will be compiled
     // based on this buildscript
-    project.extra["kotlinVersion"] = "1.3.41"
-    project.extra["agpVersion"] = "3.4.2"
+    project.extra["kotlinVersion"] = "1.3.61"
+    project.extra["agpVersion"] = "3.5.2"
     project.extra["brpVersion"] = "0.9.1"
 
     repositories {
