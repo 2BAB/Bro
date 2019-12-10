ca6e205b22bdf23448f6c332e25c4207e258da6a
2BAB <xx2bab@gmail.com>
2019-12-10 23:12:11 +0800

    release: change the version to 1.2.0

--- buildSrc/src/main/kotlin/me/xx2bab/bro/build/BuildConfig.kt
+++ buildSrc/src/main/kotlin/me/xx2bab/bro/build/BuildConfig.kt
@@ -10,7 +10,7 @@ object BuildConfig {
         const val minSdkVersion = 15
         const val targetSdkVersion = 28
 
-        const val broDevVersion = "1.2.0.4-SNAPSHOT"
+        const val broDevVersion = "1.2.0"
 
         val broSourceCompatibilityVersion = JavaVersion.VERSION_1_7
         val broTargetCompatibilityVersion = JavaVersion.VERSION_1_7
